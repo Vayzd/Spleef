082f0034eb01d485338b1db1bf315b77b31d729f
MrRumpunsh <maxschagginger@icloud.com>
2017-05-10 18:01:53 +0200

    Rearrange classpath

--- src/main/java/net/vayzd/spleef/Completable.java
+++ src/main/java/net/vayzd/spleef/Completable.java
@@ -22,7 +22,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package net.vayzd.spleef.datastore;
+package net.vayzd.spleef;
 
 /**
  * Represents a method which may be called once a result has been computed
